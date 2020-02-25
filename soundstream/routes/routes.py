0699b7358d506ba8ff251ca633a7477d5e591706
Benjamin Shanahan <benshanahan1@gmail.com>
2020-02-24 22:35:36 -0800

    Add some new visualizations

--- soundstream/routes/routes.py
+++ soundstream/routes/routes.py
@@ -5,3 +5,11 @@ from soundstream.routes import main
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/tunnel')
+@main.route('/tunnel/<string:mode>')
+def falling(mode='squares'):
+    if mode not in ['squares', 'circles', 'bars']:
+        mode = 'squares'
+    return render_template('tunnel.html', mode=mode)
