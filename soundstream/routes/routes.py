c0a739abe43abc4b5e84be087ad90f676dd3c59a
Benjamin Shanahan <benshanahan1@gmail.com>
2020-02-25 22:45:43 -0800

    Make index dynamic depending on viz templates configured

--- soundstream/routes/routes.py
+++ soundstream/routes/routes.py
@@ -1,10 +1,12 @@
-from flask import render_template
+from flask import render_template, current_app
 from soundstream.routes import main
+from soundstream.utils import get_list_of_visualizations
 
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    viz_list = get_list_of_visualizations(current_app)
+    return render_template('index.html', viz_list=viz_list)
 
 
 @main.route('/viz/<string:name>')
